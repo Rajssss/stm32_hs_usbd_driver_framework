70f139648903d1a8cd0feb45d8c9647e9c23045a
Rajssss <sssraj.sssraj@gmail.com>
2021-03-25 10:35:17 +0530

    Added USB HS gpio init handler
    -> Configure D-/D+ GPIO Pins
    
    Signed-off-by: Rajssss <sssraj.sssraj@gmail.com>

--- F407_USB_Driver/Drivers/USBD/inc/usbd_driver.h
+++ F407_USB_Driver/Drivers/USBD/inc/usbd_driver.h
@@ -11,8 +11,12 @@
 #include <stdint.h>
 #include "stm32f4xx.h"
 
-#define 	USB_OTG_HS_GLOBAL 		(USB_OTG_GlobalTypeDef* ) (USB_OTG_HS_PERIPH_BASE + USB_OTG_GLOBAL_BASE)
-#define 	USB_OTG_HS_DEVICE		(USB_OTG_DeviceTypeDef* ) (USB_OTG_HS_PERIPH_BASE + USB_OTG_DEVICE_BASE)
-#define 	USB_OTG_HS_PCGCCTL		()
+#define 	USB_OTG_HS_GLOBAL 		(USB_OTG_GlobalTypeDef *) (USB_OTG_HS_PERIPH_BASE + USB_OTG_GLOBAL_BASE)
+#define 	USB_OTG_HS_DEVICE		(USB_OTG_DeviceTypeDef *) (USB_OTG_HS_PERIPH_BASE + USB_OTG_DEVICE_BASE)
+#define 	USB_OTG_HS_PCGCCTL		(uint32_t *) (USB_OTG_HS_PERIPH_BASE + USB_OTG_PCGCCTL_BASE)
+
+//Prototypes
+void usb_gpio_init(void);
+
 
 #endif /* USBD_INC_USBD_DRIVER_H_ */
